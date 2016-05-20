ee0f17c7e470c31a07d59e9891ddbdd1684ec01f
Tim Perry <tim.r.perry@gmail.com>
2016-05-20 19:33:12 +0100

    Basic setup done with gulp and scss

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Responsive web design example
+
+## Install
+
+Clone this repo `git clone https://github.com/TimPerry/rwd-example`, change into that directory `cd rwd-example` and then run `npm install`.
+
+## Usage
+
+Run `npm start` and open up `http://localhost:8080/`.
+
+# About #
+
+This project uses gulp to serve a static webpage. The styles are done with SCSS. The default gulp task grabs the html and sass from the src folder and produces a build into the dist folder. Then dist folder is then served using the gulp connect plugin, which we also use livereload with.
---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect');
+
+gulp.task('scss', function () {
+    return gulp.src('./src/scss/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('./dist'))
+        .pipe(connect.reload());
+});
+
+gulp.task('html', function () {
+    gulp.src('./src/index.html')
+	.pipe(gulp.dest('dist'))
+	.pipe(connect.reload());
+});
+
+gulp.task('watch', ['scss', 'html'], function () {
+    gulp.watch(['./src/index.html'], ['html']);
+    gulp.watch('./sass/**/*.scss', ['scss']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+	root: 'dist',
+	livereload: true
+    });
+});
+
+gulp.task('default', ['connect', 'watch']);
